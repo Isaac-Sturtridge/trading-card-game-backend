1f895da2aa090ee338626064ad262b08f8ab6aa9
arl <arlatif@pm.me>
2024-01-02 13:44:01 +0000

    change cors port

--- app.js
+++ app.js
@@ -7,18 +7,18 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: '*',
+		origin: 'http://localhost:5173',
 	},
 });
 
-app.use(cors())
+app.use(cors());
 
-io.on("connection", (socket) => {
-    console.log("user connected")
-    console.log(socket.id)
-    socket.on("message", () => {
-        socket.emit("message", "hey")
-    })
-})
+io.on('connection', (socket) => {
+	console.log('user connected');
+	console.log(socket.id);
+	socket.on('message', () => {
+		socket.emit('message', 'hey');
+	});
+});
 
-module.exports = {server};
+module.exports = { server };
